03022009994d1c11fb87cc8cdf316c85e8e02dd9
Rob Hutters <11752302+robhutters@users.noreply.github.com>
2023-07-02 14:47:38 +0200

    ADD: feature: Home page should welcome user.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,21 +2,16 @@ import { useContext, useState,useEffect } from 'react';
 import Layout from '../Layout';
 // import './home.css';
 import { AuthContext,useAuth } from '../../context/Auth';
-import getProfile from '../../helpers/getProfile';
-import { supabase } from '../../supabaseClient';
+import getProfile from '../../helpers/getUserData';
+import IProfile from '../../interfaces/IProfile';
 
-interface IProfile {
-  username: string | null;
-  website: string | null;
-  avatar_url: string | null;
-}
 
 function Home() {
   const { user } = useAuth();
   const [profile, setProfile] = useState<IProfile>({
     username: null,
-    website: null,
-    avatar_url: null,
+    first_name: null,
+    last_name: null,
   } as IProfile);
 
   
@@ -26,9 +21,9 @@ function Home() {
   
    if (user !== null) {
     (async function () {
-      const profile = await getProfile();
+      const profile = await getProfile(user.id);
       if (profile !== undefined) {
-        setProfile(profile);
+        setProfile(profile[0]);
       }
     })();
    } 
@@ -41,7 +36,8 @@ function Home() {
     <Layout context={menu} >
       <section className="px-6">
         <p>Layout ...</p>
-        <p>User logged in? {user.id} { profile.username !== null && profile.username.length !== 0 ? `Name: ${profile.username}` : 'Profile details missing.' }</p>
+        <p><strong>User logged in?</strong> {user.id} </p>
+        <p><strong>Name:</strong> { profile.first_name !== null && profile.first_name.length !== 0 ? `${profile.first_name}` : 'Profile details missing.' }</p>
       </section>
     </Layout>
   );
