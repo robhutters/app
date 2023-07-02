95d0248116d336ea189eb12d5f1eb8926b6c34f9
Rob Hutters <11752302+robhutters@users.noreply.github.com>
2023-07-02 14:49:10 +0200

    UPDATE: Back-end logic should include user deletion from profiles table automatically. Creating a new user upon sign up should create a new user in the profiles table. Update profiles table with username, first-name, last-name.

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,13 +1,16 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useRef } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { AuthContext, useAuth } from '../../context/Auth';
+import { AuthContext } from '../../context/Auth';
 import Layout from '../Layout';
+import { supabase } from '../../supabaseClient';
 
 export function Signup() {
+  console.log('Loaded signup component ...')
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-
-  const { signUp } = useAuth();
+  const userNameRef = useRef<HTMLInputElement>(null);
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
 
   const history = useHistory();
 
@@ -15,22 +18,45 @@ export function Signup() {
 
   async function handleSubmit(e: any) {
     e.preventDefault();
-
+    console.log('Pressed submit!')
     // Get email and password input values
-    if (emailRef.current !== null && passwordRef.current !== null) {
+    if (
+      emailRef.current !== null && 
+      passwordRef.current !== null &&
+      userNameRef.current !== null &&
+      firstNameRef.current !== null && 
+      lastNameRef.current !== null
+      ) {
+        console.log('Submitting the form')
+
       const email = emailRef.current.value;
       const password = passwordRef.current.value;
 
       // Calls `signUp` function from the context
-      const user = await signUp({ email, password });
-      console.log(user);
+      const result = await supabase.auth.signUp({ email, password });
 
-      if (user.error !== null) {
-        alert('error signing in');
+
+      
+      if (result.data.user !== null) {
+        console.log(result.data.user.id)
+        const username = userNameRef.current.value;
+        const first_name = firstNameRef.current.value;
+        const last_name = lastNameRef.current.value;
+        const {data, error} = await supabase.from('profiles').update({
+         username,
+         first_name,
+         last_name
+        }).eq('user_id', result.data.user.id)
+        
+        if (error === null) {
+          history.push('/account')
+        } 
+      
       } else {
-        // Redirect user to Dashboard
-        history.push('/dashboard');
+        alert('Something went wrong during signup. Contact owner.')
       }
+
+      
     }
   }
 
@@ -39,18 +65,25 @@ export function Signup() {
 
 <Layout context={menu}>
 <section className='flex flex-row justify-center '>
-          <main className='flex flex-col w-96'>
+    <main className='flex flex-col w-96'>
       <form onSubmit={handleSubmit}>
-       <label htmlFor='input-email'>Email</label>
-       <input id='input-email' type='email' ref={emailRef} />
 
-        
+        <label htmlFor='input-username'>Gebruikersnaam</label>
+        <input id='input-username' type='text' ref={userNameRef} />
 
-       <label htmlFor='input-password'>Password</label>
-       <input id='input-password' type='password' ref={passwordRef} />
+        <label htmlFor='input-name'>Naam</label>
+        <input id='input-name' type='text' ref={firstNameRef} />
+
+        <label htmlFor='input-lastname'>Achternaam</label>
+        <input id='input-lastname' type='text' ref={lastNameRef} />
+          
+        <label htmlFor='input-email'>Email</label>
+        <input id='input-email' type='email' ref={emailRef} />
+
+        <label htmlFor='input-password'>Password</label>
+        <input id='input-password' type='password' ref={passwordRef} />
 
        <br />
-
        <button type='submit'>Sign up</button>
      </form>
 
