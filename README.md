e1e3f8a3ec7aab4f0cbf4272e6c1d044dfa9be53
Rob Hutters <11752302+robhutters@users.noreply.github.com>
2023-07-03 18:56:46 +0200

    UPDATE: Create Entity Relationship Diagram for your database. Update database with test data, following a form.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Base
 
-Every front-end application needs basic features like logging in, account management, and a dashboard with user-centric features. 
+Every front-end application needs basic features like logging in, account management, and a dashboard with user-centric features; I'm building my next app with that idea in mind. I will use branches to create templates for different kinds of apps.
 
 # de luie chef
-
 
 
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState,useContext } from 'react';
+import { useEffect, useState,useContext,useRef } from 'react';
 import { useHistory } from 'react-router';
 import { useAuth } from '../../context/Auth';
 import { AuthContext } from '../../context/Auth';
 import Layout from '../Layout';
 import IProfile from '../../interfaces/IProfile';
 import getUserData from '../../helpers/getUserData';
+import { supabase } from '../../supabaseClient';
 
 /* 
   React.memo is a higher-order component that renders the component only if props are changed.
@@ -21,16 +22,18 @@ export function Dashboard() {
 
   const menu = useContext(AuthContext);
 
+  const [loading, setLoading] = useState<boolean>(false);
 
   const history = useHistory();
 
   useEffect(() => {
     (async function () {
-      
+      setLoading(true)
       const profile = await getUserData(user.id);
-      console.log(profile)
+      
       if (profile !== undefined) {
         setProfile(profile[0]);
+        setLoading(false)
       }
     })();
   }, []);
@@ -43,18 +46,64 @@ export function Dashboard() {
     history.push('/login');
   }
 
+  async function handleSubmit(e:any) {
+    e.preventDefault()
+    const form = e.target;
+    const formData = new FormData(form)
+  
+    const formObject = Object.fromEntries(formData.entries());
+    console.log(formObject)
+
+    const { error } = await supabase
+    .from('recipes')
+    .insert({
+      user_id: user.id,
+      recipename: formObject.recipeName,
+      byline: formObject.byline,
+      labels: [formObject.labels],
+      description: formObject.description
+    })
+
+    if (error) {
+      alert('Error occurred!')
+    } else {
+      alert('Entry uploaded!')
+    }
+    
+  }
+
   return (
     <div>
       <Layout context={menu}>
           {/* Change it to display the user ID too 👇*/}
-      <section  className='flex flex-col px-6 max-w-xl mx-auto' >
+      <section  className='flex flex-col  ' >
 
-        <main className=' border-2'>
+        <main className='flex flex-row justify-around'>
+          <section>
           <p>Welcome, {profile?.first_name}!</p>
           <p>Your user id is {user.id}</p>
-      
-
           <button onClick={handleSignOut}>Sign out</button>
+          </section>
+         
+          <section className="flex flex-col">
+            <form method="post" id="submitRecipeForm" onSubmit={handleSubmit}>
+                <label htmlFor='recipeName'>Geef je recept een naam:</label>
+                <input name='recipeName'  className="my-3" type='text'  />
+                <label htmlFor='byline'>Voor onder de titel:</label>
+                <input name='byline' className="my-3" type='text'  />
+                <label htmlFor='labels'>Labels (b.v. vegan):</label>
+                <input name='labels' className="my-3" type='text'  />
+                <label htmlFor='description' >Beschrijving:</label>
+                <br />
+                <textarea name='description' className="my-3 w-full border-2 border-black" rows={5}  wrap="soft" />
+                
+                <button type='submit' disabled={loading}>
+                {loading ? 'Loading...' : 'Submit'}</button>
+              </form>
+              
+          </section>
+
+          
         </main>
       </section>
       </Layout>
