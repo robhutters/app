c9bddc23f2b2d017204e78172c16ac55bb2df480
Rob Hutters <11752302+robhutters@users.noreply.github.com>
2023-07-19 11:37:50 +0200

    ADD: If there are no recipes to show, give warning to mobile user.

--- src/components/Mobile/MobileViewRecipeCard.tsx
+++ src/components/Mobile/MobileViewRecipeCard.tsx
@@ -2,22 +2,28 @@
 import MobileRecipeLayout from "./MobileRecipeLayout";
 
 export function MobileViewRecipeCard ({ recipe } : { recipe: any}) {
-  return (
-    <div className="flex flex-col flex-grow border-2 border-black">
-    
-        <section className="p-3 flex flex-col flex-grow">
-         
-          
-          {/* main takes up all available space thanks to flex-auto. Flex container with flex-grow required. Parents included.  */}
-          <main className="flex-auto">
+  if (recipe !== null && recipe !== undefined) {
+    return (
+      <div className="flex flex-col flex-grow border-2 border-black">
+      
+          <section className="p-3 flex flex-col flex-grow">
+           
             
-            <MobileRecipeLayout recipe={recipe} />
-          </main>
+            {/* main takes up all available space thanks to flex-auto. Flex container with flex-grow required. Parents included.  */}
+            <main className="flex-auto">
+              
+              <MobileRecipeLayout recipe={recipe} />
+            </main>
+        
+          </section>
       
-        </section>
-    
-      
-
-    </div>
-  );
+        
+  
+      </div>
+    );
+  } else {
+    return (
+      <div>No recipes in database.</div>
+    )
+  }
 };
